85045fb1feca4f120f58831336b7b72a1d352a52
santiagoa50 <santiago.an@northeastern.edu>
2023-03-26 02:35:11 -0400

    a7 Redux sp23

--- src/tuiter/edit-profile/index.js
+++ src/tuiter/edit-profile/index.js
@@ -1,68 +1,83 @@
 import React from "react";
 import {Link} from "react-router-dom";
-
+import "./index.css"
 const EditProfileComponent = () => {
 
     return(
         <>
-            <li className="list-group-item pe-3 wd-border">
-                <div className="pt-2 ps-0 pe-0">
-                    <div className="row pt-1">
-                        <div className="col-1">
-                            <Link to="/tuiter/profile" className="text-secondary">
-                                <i className="bi bi-x-lg"></i>
-                            </Link>
-                        </div>
-                        <div className="col-10">
-                            <h5 className="m-0 fw-bold">Han Solo</h5>
-                            <p className="m-0 text-secondary">6,114 Tuits</p>
-                        </div>
-
-                        <div className="position-relative wd-margin">
-                            <img className= "img-responsive mt-1 wd-banner"
-                                 src={`../../images/trio.jpeg`} alt= "img"/>
-                            <img className="rounded-pill wd-profile-picture"
-                                 src={`../../images/han solo.jpg`} alt= "img"/>
-                            <Link to="/tuiter/edit-profile">
-                                <button className="rounded-pill btn btn-light float-end mt-3 me-3 ps-3 pe-3">
-                                    Edit profile
-                                </button>
-                            </Link>
-                        </div>
-
-
-                        <div className="ps-3">
-                            <h5 className="m-0 fw-bold">Han Solo</h5>
-                            <p className="m-0 text-secondary">@hansolo</p>
-                            <p className="mt-3 mb-3">Charming former smuggler with a heart of gold.</p>
-
-                            <div className="row mb-2">
-                                <div className="col-auto">
-                                    <i className="bi bi-geo-alt" id="location"></i>
-                                    <div className="d-inline ms-2 text-secondary">Corellia</div>
-                                </div>
-                                <div className="col-auto">
-                                    <i className="bi bi-balloon"></i>
-                                    <div className="d-inline ms-2 text-secondary">Born 32 BBY</div>
-                                </div>
-                                <div className="col-auto">
-                                    <i className="bi bi-calendar3"></i>
-                                    <div className="d-inline ms-2 text-secondary">Joined 31 BBY</div>
-                                </div>
-                            </div>
-
-                            <div className="row mb-3">
-                                <div className="col-auto">
-                                    <div className="d-inline text-secondary">340 Following</div>
-                                </div>
-                                <div className="col-auto">
-                                    <div className="d-inline text-secondary">223 Followers</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div className="container p-0 pb-1 mt-2 border border-light rounded-2">
+                <Link to="/tuiter/profile">
+                    <button className="rounded-pill btn btn-dark float-end  ps-3 pe-3 pt-1">
+                        Save
+                    </button>
+                </Link>
+            </div>
+            <div className="row ms-3 ">
+                <div className="col-1">
+                    <Link to="/tuiter/profile" className="text-secondary">
+                        <i className="bi bi-x-lg"></i>
+                    </Link>
                 </div>
-            </li>
+                <div className="col-11">
+                    <h5 className="m-0 ms-2 fw-bold">Edit Profile</h5>
+                </div>
+            </div>
+            <div className="position-relative wd-margin-edit-profile">
+                <img className= "img-responsive mt-1 wd-banner"
+                     src={`../../images/trio.jpeg`} alt= "img"/>
+                <img className="rounded-pill wd-edit-profile-picture"
+                     src={`../../images/han solo.jpg`} alt= "img"/>
+            </div>
+            <div className="mb-4">
+                <div className="container p-2 pt-0 border bg-light rounded-2">
+                    <label form="name" className="small text-secondary">
+                        Name
+                    </label><br/>
+                    <textarea className="wd-text-box bg-light border-0" value="Han Solo"/>
+                </div>
+            </div>
+            <div className="mb-4">
+                <div className="container p-2 pt-0 border bg-light rounded-2">
+                    <label form="bio" className="small text-secondary">
+                        Bio
+                    </label><br/>
+                    <textarea className="wd-text-box bg-light border-0"
+                              value="Charming former smuggler with a heart of gold."/>
+                </div>
+            </div>
+
+            <div className="mb-4">
+                <div className="container p-2 pt-0 border bg-light rounded-2">
+                    <label form="location" className="small text-secondary">
+                        Location
+                    </label><br/>
+                    <textarea className="wd-text-box bg-light border-0"
+                              value="Corellia"/>
+                </div>
+            </div>
+            <div className="mb-4">
+                <div className="container p-2 pt-0 border bg-light rounded-2">
+                    <label form="website" className="small text-secondary">
+                    </label> <br/>
+                    <textarea className="wd-text-box bg-light border-0 text-secondary"
+                        value="Website"/>
+                </div>
+            </div>
+            <div className="mb-4">
+                <div className="container p-2 pt-0 border bg-light rounded-2">
+                    <label form="birthdate_input" className="small text-secondary">
+                        Birth date ·<b> Edit</b>
+                        <textarea className="wd-text-box bg-light border-0"
+                                  value="32 BBY"/>
+                    </label>
+                </div>
+            </div>
+            <div className="mb-4">
+                <Link to="#" className="text-decoration-none">
+                    <h6 className="d-inline text-black">Switch to professional</h6>
+                    <i className="bi bi-caret-right float-end text-black"></i>
+                </Link>
+            </div>
         </>
     );
 };
